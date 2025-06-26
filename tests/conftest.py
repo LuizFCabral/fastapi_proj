dd1f4fc053f5688800b9c548b50e987325f3ce20
Luiz Felipe Cabral de Souza [USL - SULBRAS] <luiz.souza@sulbras.com.br>
2025-06-26 17:34:37 -0300

    alembic

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,9 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy import event
+from datetime import datetime
+from contextlib import contextmanager
 
 from fastapi_proj.app import app
 from fastapi_proj.models import table_registry
@@ -22,3 +25,21 @@ def session():
         yield session
 
     table_registry.metadata.drop_all(engine)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2025, 6, 26)):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
