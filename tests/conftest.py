1b2c7f85ec1b416ac664f3b87df4a946401c13d4
LuizFCabral <luizfelipe.cabralsouza@gmail.com>
2025-10-02 17:14:15 -0300

    :sparkles: feat: Iniciando com o factory-boy

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from datetime import datetime
 
+import factory
 import pytest
 import pytest_asyncio
 from fastapi.testclient import TestClient
@@ -95,3 +96,12 @@ def token(client, user):
 @pytest.fixture
 def settings():
     return Settings()
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'test{n}')
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
+    password = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
