7c952db3be31691389d801c944d935af7cc1ba5c
LuizFCabral <luizfelipe.cabralsouza@gmail.com>
2025-11-25 15:39:44 -0300

    First deploy

--- fastapi_proj/app.py
+++ fastapi_proj/app.py
@@ -9,7 +9,7 @@ from fastapi_proj.routers import auth, todos, users
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-app = FastAPI(title='Test Infog')
+app = FastAPI(title='FastAPI Project')
 
 app.include_router(auth.router)
 app.include_router(users.router)
